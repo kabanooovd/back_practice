7f3b78e5030a3a26554c9be5e7549ec85da72953
dimas <kabanooovd@yandex.ru>
2022-07-12 21:04:28 +0300

    fixes 1

--- src/routes/videosRts.ts
+++ src/routes/videosRts.ts
@@ -48,11 +48,36 @@ videosRouter.delete("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const foundId = videos.find((item) => +id === item.id);
   if (!foundId) {
-    errorHandler(res, 400, "No title", "VIDEOS");
+    res.status(404).send();
   }
-  const newVideoList = videos.filter((vidos) => vidos.id !== +id);
-  res.status(204).send("video has been removed");
+  // const newVideoList = videos.filter((vidos) => vidos.id !== +id);
+  const foundElement = videos.find((el) => el.id === +id);
+  if (foundElement) {
+    const currentIndex = videos.indexOf(foundElement);
+    videos.splice(currentIndex, currentIndex + 1);
+    console.log(videos);
+    res.status(204).send("video has been removed");
+  }
 });
+
+// videosRouter.put("/:id", (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const { title } = req.body;
+//   const foundId = videos.find((item) => +id === item.id);
+//   if (!foundId) {
+//     res.status(404).send("Not Found");
+//   }
+//   if (!title) {
+//     errorHandler(res, 400, "No title", "VIDEOS");
+//   }
+//   if (title.length > 40) {
+//     errorHandler(res, 400, "title has more then 40 characters", "VIDEOS");
+//   }
+//   const newVideoList = videos.forEach((el) =>
+//     el.id === +id ? { ...el, title } : el
+//   );
+//   res.status(204).json(newVideoList);
+// });
 
 videosRouter.put("/:id", (req: Request, res: Response) => {
   const { id } = req.params;
@@ -67,10 +92,21 @@ videosRouter.put("/:id", (req: Request, res: Response) => {
   if (title.length > 40) {
     errorHandler(res, 400, "title has more then 40 characters", "VIDEOS");
   }
-  const newVideoList = videos.map((el) =>
-    el.id === +id ? { ...el, title } : el
-  );
-  res.status(204).json(newVideoList);
+
+  const foundElement = videos.find((el) => el.id === +id);
+
+  if (foundElement) {
+    const updatedItem = {
+      id: foundElement.id,
+      title,
+      author: foundElement.author,
+    };
+    const currentIndex = videos.indexOf(foundElement);
+    videos.splice(currentIndex, currentIndex + 1);
+    videos.push(updatedItem);
+  }
+
+  res.status(204).send("KRUTO");
 });
 
 export default videosRouter;
