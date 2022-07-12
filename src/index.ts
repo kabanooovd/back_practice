63c675db15e44fea078cc550598cba4a41d7fe30
dimas <kabanooovd@yandex.ru>
2022-07-12 21:22:43 +0300

    Merge branch 'INC_1111' into HEAD

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use("/videos", videosRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("DIMAAAAAASSS");
+  res.send("DIMAAAAAASSS !!!");
 });
 
 app.listen(port, () => {
