36c5ce21f97efffca1c5582018cd914210d3988e
dimas <kabanooovd@yandex.ru>
2022-07-12 22:41:41 +0300

    111

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use("/videos", videosRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("DIMAAAAAASSS !!!");
+  res.send("DIMAAAAAASSS !!! diiiiiiiimas");
 });
 
 app.listen(port, () => {
